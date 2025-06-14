81f3358edaacef89b0627b4d0ac662aacfbe14c0
Aryanparashara <aryansharma33687@gmail.com>
2025-06-14 13:20:26 +0530

    Create check greater number.c

---
+++ check greater number.c
@@ -0,0 +1,16 @@
+/*#include<stdio.h>
+void main()
+{
+  int a,b;
+printf("\n enter two no.s");
+scanf("%d %d,&a,&b);
+  if (a>b)
+  {
+  printf("\n a is greater");
+}
+else
+{
+printf("|n b is gerater");
+}
+}*/
+  
