77674c297cc94f9ddebae6ae7bce69c1f36bdc1b
Aryanparashara <aryansharma33687@gmail.com>
2025-06-14 14:12:59 +0530

    Create Total marks of 5 subjects.c

---
+++ Total marks of 5 subjects.c
@@ -0,0 +1,17 @@
+/*#include<stdio.h>
+void main()
+{
+  int a,b,c,d,e,f;
+  printf("\n enter marks of hindi");
+  scanf("%d",&a);
+  printf("\n enter marks of english");
+  scanf("%d",&b);
+  printf("\n enter marks of math");
+  scanf("%d",&c);
+  printf("\n enter marks of science");
+  scanf("%d",&d);
+  printf("\n enter marks of computer");
+  scanf("%d",&e);
+  f=a+b+c+d+e;
+  printf("\n sum is %d",f);
+}*/
