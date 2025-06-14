0afbf28a7620f0b3749f8539a3818d8e13326bdf
Aryanparashara <aryansharma33687@gmail.com>
2025-06-14 13:28:49 +0530

    Create To check 10%discount if bill amount>5000 .c

---
+++ To check 10%discount if bill amount>5000 .c
@@ -0,0 +1,19 @@
+/*#include<stdio.h>
+void main()
+{
+  int billamount,discount,finalbill;
+printf(\n "enter the billamount");
+scanf("%d",&billamount);
+if (billamount >= 5000)
+{
+discount=(billamount*10)/100;
+printf("\n discount is %d",discount);
+finallbill=billamount-discount;
+printf("\n finalbill is %d",finalbill);
+}
+else
+{
+printf("\n no discount ");
+}
+}*/
+
