86dadf27a2341efc1cdac1014bcbc5bfb2984da0
Aryanparashara <aryansharma33687@gmail.com>
2025-06-14 14:18:02 +0530

    Create calculate basicsalary.c

---
+++ calculate basicsalary.c
@@ -0,0 +1,19 @@
+/*#include<stdio.h>
+void main()
+{
+  int tada,hra,medical,pf,basicsalary,net,gross;
+  printf("\n enter the basicsalary");
+  scanf("%d",&basicsalary);
+tada=(basicsalary*30)/100;
+printf("\n tada of basicsalary is %d",tada);
+hra=(basicsalary*50)/100;
+printf("\n hra of basicsalary is %d",hra);
+medical=(basicsalary*20)/100;
+printf("\n medical of basicsalary is %d",medical);
+pf=(basicsalary*15)/100;
+printf("\n pf of basicsalary is %d",pf);
+net=basicsalary+tada+hra+medical+pf;
+printf("\n net salary is %d",net);
+gross=net-pf;
+printf("\n gross salary is %d",gross);
+}*/
