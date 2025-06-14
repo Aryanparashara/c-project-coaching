d8f5a30a877143251e58ebe4bc38b9a085601733
Aryanparashara <aryansharma33687@gmail.com>
2025-06-14 14:15:20 +0530

    Create calculate volume of cone.c

---
+++ calculate volume of cone.c
@@ -0,0 +1,11 @@
+/*#include<stdio.h>
+void main()
+{
+  int r,h,voc;
+  printf("\n enter the radius");
+  scanf("%d",&r);
+  printf("\n enter the height");
+  scanf("%d",&h);
+  voc=(3.14*r*r*h)/3;
+  printf("\n volume of cone is %d",voc);
+}*/
