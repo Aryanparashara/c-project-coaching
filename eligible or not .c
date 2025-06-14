52c5854c3017563f396d9ab741fb91ad870b02c9
Aryanparashara <aryansharma33687@gmail.com>
2025-06-14 13:11:53 +0530

    Update eligible or not .c

--- eligible or not .c
+++ eligible or not .c
@@ -1,10 +1,10 @@
-#include<stdio.h.
+/*#include<stdio.h>
 void main ()
 {
   int age;
 printf("\n enter age of a person");
 scanf("%d",&age);
-if (age >= 18):
+if (age >= 18)
 {
 printf("\n eligible ");
 }
@@ -12,4 +12,21 @@ else
 {
 printf("\n not eligible ");
 }
+}*/
+/*#include<stdio.h>
+void main ()
+{
+  int per;
+printf("\n enter the percentage");
+scanf("%d",&per);
+if ( per >= 90 )
+{
+printf("\n eligible for scholarship");
 }
+else
+{
+printf("\n not eligible for scholarship");
+}
+}*/
+  
+
