23520afb80c99fefc4580935f6608ec1793cda4e
Aryanparashara <aryansharma33687@gmail.com>
2025-06-14 13:02:11 +0530

    Create eligible for voting.c

---
+++ eligible for voting.c
@@ -0,0 +1,15 @@
+#include<stdio.h.
+void main ()
+{
+  int age;
+printf("\n enter age of a person");
+scanf("%d",&age);
+if (age >= 18):
+{
+printf("\n eligible ");
+}
+else
+{
+printf("\n not eligible ");
+}
+}
