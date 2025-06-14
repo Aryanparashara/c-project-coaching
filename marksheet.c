215feac5669f432d5f1ba5b5fec8772135edb17f
Aryanparashara <aryansharma33687@gmail.com>
2025-06-14 14:26:51 +0530

    Create marksheet.c

---
+++ marksheet.c
@@ -0,0 +1,27 @@
+/*#include<stdio.h>
+void main()
+{
+  int a,b,c,d,e,f,g;
+  printf("\n central board of secondary education");
+  printf("\n              2025-26                ");
+  printf("\n ____________________________________");
+  printf("\n   sub     sub      max      marks   ");
+  printf("\n   code    name    marks    obtained ");
+  printf("\n ____________________________________");
+  printf("\n   101    English   100      ");
+  scanf("%d",&a);
+  printf("\n   102     Math     100      ");
+  scanf("%d",&b);
+  printf("\n   103    Computer  100      ");
+  scanf("%d",&c);
+  printf("\n   104     Hindi    100      ");
+  scanf("%d",&d);
+  printf("\n   105    Science   100      ");
+  scanf("%d",&e);
+  printf("\n   106     Arts     100      ");
+  scanf("%d",&f);
+  g=a+b+c+d+e+f;
+  printf("\n ____________________________________");
+  printf("\n  Total Marks out of 600  -  %d",g);
+  printf("\n ____________________________________");
+}*/
