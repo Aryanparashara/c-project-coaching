7c9827106da0bdd563fe0eccfd58d2bbffa37df8
Aryanparashara <aryansharma33687@gmail.com>
2025-06-14 13:53:49 +0530

    Create add,sub,product,division 2 numbers .c

---
+++ add,sub,product,division 2 numbers .c
@@ -0,0 +1,15 @@
+/*#include<stdio.h>
+void main()
+{
+  int a,b,c,d,e,f;
+printf("\n enter two no.s ");
+scanf("%d %d",&a,&b);
+  c=a+b;
+printf("\n sum is %d",c);
+d=a-b;
+printf("\n sub is %d",d);
+e=a*b;
+printf("\n product is %d",e);
+f=a/b;
+printf("\n division is %d",f);
+}*/
