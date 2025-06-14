5f9db8ca7d9efec518af6fb4e7ba61fa84dbfb65
Aryanparashara <aryansharma33687@gmail.com>
2025-06-14 14:23:12 +0530

    Create calculate bill amount.c

---
+++ calculate bill amount.c
@@ -0,0 +1,17 @@
+/*#include<stdio.h>
+void main()
+{
+  int cgst,sgst,billamount,quantity,totalbill;
+  printf("\n enter the quantity");
+  scanf("%d",&quantity);
+  printf("\n enter the price");
+  scanf("%d",&price);
+  billamount=(quantity*price);
+  printf("\n billamount is %d",billamount);
+  cgst=(billamount*9)/100;
+  printf("\n cgst is %d",cgst);
+  sgst=(billamount*9)/100;
+  printf("\n sgst is %d",sgst);
+  totalbill=(sgst+cgst+billamount);
+  printf("\n totalbill is %d",totalbill);
+}*/
