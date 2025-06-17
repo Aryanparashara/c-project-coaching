94736f532874733ddc1e5769b3ef79dd92e9a3dd
Aryanparashara <aryanparashara01@gmail.com>
2025-06-17 10:33:11 +0530

    Update and rename per of scholarship if else.c to 11jun25 percentage.c

---
+++ 11jun25 percentage.c
@@ -0,0 +1,50 @@
+/*#include<stdio.h>
+int main()
+{
+  int per;
+  printf("\n enter the percentage ");
+  scanf("%d",&per);
+  if (per>=90)
+  {
+    printf("\n 100per. of scholarship ");
+  }
+  else if ((per>=60)&&(per<=90))
+  {
+    printf("\n 60per. of scholarship ");
+  }
+else if ((per>=40)&&(per<=60))
+  {
+    printf("\n 40per. of scholarship ");
+  }
+else
+{
+  printf("\n no scholarship ");
+}
+}*/
+
+/*#include<stdio.h>
+int main ()
+{
+  float com,sales;
+  printf("\n enter the sales ");
+  scanf("%f",&sales);
+  if (sales>=5000)
+  {
+    com=(sales*5)/100;
+    printf("\n commision will be %f",com );
+  }
+  else if ((sales>=3000)&&(sales<5000))
+  {
+    com=(sales*3)/100;
+    printf("\n comission will be %f",com);
+  }
+else if ((sales>=1000)&&(sales<3000))
+{
+  com=(sales*1)/100;
+  printf("\n commision will be %f",com);
+}
+else
+{
+  printf("\n you will get no commision ");
+}
+}*/
