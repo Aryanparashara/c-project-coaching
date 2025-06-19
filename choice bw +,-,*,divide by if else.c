0f87205f42136a97f1b1e65cd72c5fd611b5cb62
Aryanparashara <aryanparashara01@gmail.com>
2025-06-19 22:40:12 +0530

    Create choice bw +,-,*,divide by if else.c

---
+++ choice bw +,-,*,divide by if else.c
@@ -0,0 +1,40 @@
+/*#include<stdio.h>
+int main()
+{
+  int a,b,c,d,e,f,choice,choice1,choice2,choice3,choice4;
+  printf("\n choice1 is sum,choice2 is sub,choice3 is product,choice4 is division ");
+  printf("\n enter the choice");
+  scanf("%d",&choice);
+  if (choice==1)
+  {
+    printf("\n enter two no.s ");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("\n sum is %d",c);
+  }
+  else if (choice==2)
+  {
+    printf("\n enter two no,s");
+    scanf("%d%d",&a,&b);
+    d=a-b;
+    printf("\n sub is %d",d);
+  }
+  else if (choice==3)
+  {
+  printf("\n enter two no.s");
+  scanf("%d%d",&a,&b);
+  e=a*b;
+  printf("\n product is %d",e);
+  }
+  else if (choice==4)
+  {
+    printf("\n enter two no.s");
+    scanf("%d%d",&a,&b);
+    f=a/b;
+    printf("\n division is %d",f);
+  }
+  else 
+  {
+    printf("\n choose b/w 1,2,3,4 ");
+  }
+}*/
