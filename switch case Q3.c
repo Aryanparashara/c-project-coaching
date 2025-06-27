779cf8a4355221ffca56cce200176cc985003816
Aryanparashara <aryanparashara01@gmail.com>
2025-06-27 10:45:53 +0530

    Create switch case Q3.c

---
+++ switch case Q3.c
@@ -0,0 +1,59 @@
+/*#include<stdio.h>
+int main()
+{
+  int choice,aoc,r,p,t,si,voc,h,c,a,s;
+  printf("\n enter 1 for area of circle ");
+  printf("\n enter 2 for simple interest ");
+  printf("\n enter 3 for volume of cone ");
+  printf("\n enter 4 for cube of any no. ");
+  printf("\n enter 5 for square of any no. ");
+  printf("\n enter 6 for exit ");
+  printf("\n enter your choice ");
+  scanf("%d",&choice);
+  if (choice==1)
+  {
+    printf("\n enter the radius ");
+    scanf("%d",&r);
+    aoc=(3.14*r*r);
+    printf("\n area of circle is %d",aoc);
+  }
+  else if (choice==2)
+  {
+    printf("\n enter the principle amount ");
+    scanf("%d",&p);
+    printf("\n enter the rate ");
+    scanf("%d",&r);
+    printf("\n enter the time ");
+    scanf("%d",&t);
+    si=(p*r*t)/100;
+    printf("\n simple interest is %d",si);
+  }
+  else if (choice==3)
+  {
+    printf("\n enter the radius ");
+    scanf("%d",&r);
+    printf("\n enter the height ");
+    scanf("%d",&h);
+    voc=(3.14*r*r*h)/3;
+    printf("\n volume of cone is %d",voc);
+  }
+  else if (choice==4)
+  {
+    printf("\n enter the no. ");
+    scanf("%d",&a);
+    c=(a*a*a);
+    printf("\n cube is %d",c);
+  }
+  else if (choice==5)
+  {
+    printf("\n enter the no. ");
+    scanf("%d",&a);
+    s=(a*a);
+    printf("\n square is %d",s);
+  }
+  else 
+  {
+    printf("\n NOT VALID CHOICE ");
+    printf("\n 1,2,3,4,5,6 ");
+  }
+}*/
