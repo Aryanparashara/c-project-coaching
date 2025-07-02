d42ecfea7705ca81e55f1170627049b55f9ec8c1
Aryanparashara <aryanparashara01@gmail.com>
2025-07-02 21:05:22 +0530

    Create typecasting.c

---
+++ typecasting.c
@@ -0,0 +1,17 @@
+// Without Type casting.
+/*#include<stdio.h>
+int main()
+{
+  int a;
+  a=15/6;
+  printf("%d",a);
+}*/   
+
+// After Type casting.
+/*#include<stdio.h>
+int main()
+{
+  float a;
+  a=(float)15/6;
+  printf("%f",a);
+}*/
