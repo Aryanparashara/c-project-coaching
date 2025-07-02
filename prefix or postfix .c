1317ba321b3e866800ef98d5ae112dd5b4487591
Aryanparashara <aryanparashara01@gmail.com>
2025-07-02 10:07:26 +0530

    Create prefix or postfix .c

---
+++ prefix or postfix .c
@@ -0,0 +1,11 @@
+/*#include<stdio.h>
+int main ()
+{
+  int a=5,b=5;
+  // use postfix mode for a and prefix mode for b
+  printf("\n %8d%10d",a--,--b);
+  printf("\n %8d%10d",a--,--b);
+  printf("\n %8d%10d",a--,--b);
+  printf("\n %8d%10d",a--,--b);
+  printf("\n %8d%10d",a--,--b);
+}*/
