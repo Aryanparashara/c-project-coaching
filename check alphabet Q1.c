34461694529e29436313d5ba1388e54e47156a0a
Aryanparashara <aryanparashara01@gmail.com>
2025-07-03 21:28:09 +0530

    Create check alphabet Q1.c

---
+++ check alphabet Q1.c
@@ -0,0 +1,23 @@
+/*#include<stdio.h>
+int main()
+{
+  char c;
+  printf(" Enter the character ");
+  scanf("%c",&c);
+  if ((c>='a')&&(c<='z'))
+  {
+    printf(" %c is small alphabet. ",c);
+  }
+  else if ((c>='A')&&(c<='Z'))
+  {
+    printf(" %c is capital letter. ",c);
+  }
+  else if ((c>='0')&&(c<='9'))
+  {
+    printf(" %c is number key. ",c);
+  }
+  else 
+  {
+    printf(" %c is an special key. ",c);
+  }
+}*/
