74dd70bc4c8dc89451b74fc2d3a2d15bc54400c1
Aryanparashara <aryanparashara01@gmail.com>
2025-08-01 11:16:28 +0530

    Update first 10 odd numbers.c

--- first 10 odd numbers.c
+++ first 10 odd numbers.c
@@ -3,10 +3,12 @@
 /*#include<stdio.h>
 int main()
 {
-  int i;
+  int i,sum=0;
   printf("\n first 10 odd numbers: ");
-  for(i=1;i<=10;i++)
+  for(i=1;i<=20;i=i+2)
   {
-    printf("\n %d ",2 * i - 1);
+    printf("\n %d ",i);
+  sum = sum+i;
   }
+  printf("\n sum is %d",sum);
 }*/
