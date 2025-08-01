ce6a36c20e2f3c251819f27593d2b83c4ad2f380
Aryanparashara <aryanparashara01@gmail.com>
2025-08-01 11:15:58 +0530

    Update first 10 even numbers.c

--- first 10 even numbers.c
+++ first 10 even numbers.c
@@ -3,10 +3,12 @@
 /*#include<stdio.h>
 int main()
 {
-  int i;
+  int i,sum=0;
   printf("\n first 10 even numbers: ");
-  for(i=1;i<=10;i++)
+  for(i=2;i<=20;i=i+2)
   {
-    printf("\n %d ",2 * i);
+    printf("\n %d ",i);
+    sum=sum+i;
   }
+  printf("\n sum is %d",sum);
 }*/
