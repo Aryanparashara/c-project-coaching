67688cede1220b63725a38a9908f2494c78b6ae8
Aryanparashara <aryanparashara01@gmail.com>
2025-08-01 11:15:10 +0530

    Update frist 10 natural numbers.c

--- frist 10 natural numbers.c
+++ frist 10 natural numbers.c
@@ -3,11 +3,12 @@
 /*#include<stdio.h>
 int main()
 {
-  int i;
+  int i,sum=0;
   printf("\n first 10 natural numbers: ");
   for(i=1;i<=10;i++)
   {
-    printf("%d ",i);
+    printf(" \n %d ",i);
+  sum=sum+i;
   }
+  printf("\n sum is %d",sum);
 }*/
-
