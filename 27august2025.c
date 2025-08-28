c9758ed1f9e79baee967bfdc17ed87e009bce608
Aryanparashara <aryanparashara01@gmail.com>
2025-08-28 11:29:40 +0530

    Create 27august2025.c

---
+++ 27august2025.c
@@ -0,0 +1,33 @@
+/*#include<stdio.h>
+int evenodd(int x)
+{
+  if(x%2==0)
+  printf("\n it is a even number ");
+  else 
+  printf("\n it is a odd number ");
+}
+int main()
+{
+  int x;
+  printf("\n enter the no. ");
+  scanf("%d",&x);
+  evenodd(x);
+  return 0;
+}*/
+/*#include<stdio.h>
+evenodd();
+int main()
+{
+  evenodd();
+}
+evenodd()
+{
+  int a;
+  printf("\n enter any number ");
+  scanf("%d",&a);
+  if(a%2==0)
+  printf("\n even ");
+  else 
+  printf("\n odd ");
+  return 0;
+}*/
