00882dc0b6e445107143b6cccc8709cdbc0e74e2
Aryanparashara <aryanparashara01@gmail.com>
2025-09-03 13:29:02 +0530

    Create 29august2025.c

---
+++ 29august2025.c
@@ -0,0 +1,21 @@
+/*#include<stdio.h>
+int add(int,int);
+float average(int,int);
+int main()
+{
+  int n1,n2,sum;
+  float avg;
+  printf("\n Enter three numbers ");
+  scanf("%d%d",&n1,&n2);
+  printf("\n Number1-%d,Number2-%d",n1,n2);
+  sum=add(n1,n2);
+  avg=average(n1,n2);
+  printf("\n sum: %d,Average: %f\n",sum,avg);
+  return 0;
+}
+int add(int x,int y)
+{
+  return x+y;
+float average(int x,int y);
+return((float)(x)+(float)(y))/2;
+}*/
