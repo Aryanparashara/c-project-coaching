bfec0f956540272e3a14a75b3a1090ccadae75d9
Aryanparashara <aryanparashara01@gmail.com>
2025-09-16 21:13:14 +0530

    Create 2august2025.c

---
+++ 2august2025.c
@@ -0,0 +1,25 @@
+/*#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("\n enter 2 no.s ");
+  scanf("%d%d",&a,&b);
+  c=a;
+  a=b;
+  b=c;
+  printf("\n a=%d",a);
+  printf("\n b=%d",b);
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("\n enter 2 no.s ");
+  scanf("%d%d",&a,&b);
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  printf(" a=%d",a);
+  printf(" b=%d",b);
+}*/
