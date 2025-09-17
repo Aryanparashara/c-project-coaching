2f42bf5af837da93765e1bd206c1b50f22272c6f
Aryanparashara <aryanparashara01@gmail.com>
2025-09-17 20:56:13 +0530

    Create 5sept2025.c

---
+++ 5sept2025.c
@@ -0,0 +1,49 @@
+/*#include<stdio.h>
+#include<stdlib.h>
+int a,b;
+void number()
+{
+  printf("\n Enter 2 no.s ");
+  scanf("%d%d",&a,&b);
+}
+int main()
+{
+  int c,d,e,f,ch;
+  printf("\n enter 1 for addition ");
+  printf("\n enter 2 for subtraction ");
+  printf("\n enter 3 for product ");
+  printf("\n enter 4 for division ");
+  printf("\n enter 5 for exit ");
+  printf("\n enter your choice ");
+  scanf("%d",&ch);
+  switch(ch)
+  {
+    case 1:
+    number();
+    c=a+b;
+    printf("\n sum is %d",c);
+    break;
+    case 2:
+    number();
+    d=a-b;
+    printf("\n sub is %d",d);
+    break;
+    case 3:
+    number();
+    e=a*b;
+    printf("\n product is %d",e);
+    break;
+    case 4:
+    number();
+    f=a/b;
+    printf("\n division is %d",f);
+    break;
+    case 5:
+    exit(0);
+    break;
+    default:
+    printf("\n Invalid choice ");
+    printf("\n enter 1,2,3,4,5 ");
+    break;
+  }
+}*/
