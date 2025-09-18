64523b9a4668a76479befc74df3a9f0d5ea9554f
Aryanparashara <aryanparashara01@gmail.com>
2025-09-18 21:12:34 +0530

    Create 15sept2025.c

---
+++ 15sept2025.c
@@ -0,0 +1,18 @@
+/*#include<stdio.h>
+#include<string.h>
+void main()
+{
+  char a[20];
+  printf("\n Enter any string ");
+  gets(a);
+  printf("\n Reverse of string is %s",strrev(a));
+}*/
+/*#include<stdio.h>
+#include<string.h>
+void main()
+{
+  char a[20];
+  printf("\n Enter any string ");
+  gets(a);
+  printf("\n length of string is %d",strlen(a));
+}*/
