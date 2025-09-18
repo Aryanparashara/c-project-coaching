9568f84eed0946196682eb9bfc162d92467715b7
Aryanparashara <aryanparashara01@gmail.com>
2025-09-18 21:11:39 +0530

    Create 12sept2025.c

---
+++ 12sept2025.c
@@ -0,0 +1,9 @@
+/*#include<stdio.h>
+void main()
+{
+  char fname[20],mname[20],lname[20];
+  printf("\n Enter first,middle and last name ");
+  scanf("%c%c%c",&fname,&mname,&lname);
+  printf("\n Abbreviated name ");
+  printf("%c%c%c",fname[0],mname[0],lname[0]);
+}*/
