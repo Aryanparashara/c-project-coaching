55dd3234f8dcc2f34ed81f9f188a5a5e337873ec
Aryanparashara <aryanparashara01@gmail.com>
2025-09-18 21:11:14 +0530

    Create 11sept2025.c

---
+++ 11sept2025.c
@@ -0,0 +1,15 @@
+/*#include<stdio.h>
+void main(void)
+{
+  int num,sum=0,rem;
+  printf("\n Enter a number ");
+  scanf("%d",&num);
+  // keep diving until the number is not zero 
+  while(num!=0)
+  {
+    rem=num%10;
+    sum=sum+rem;
+    num=num/10;
+  }
+  printf("\n sum of digits of the numbers is %d",sum);
+}*/
