b74d73f78a664d41fc4b0fe0de146f11824262c9
Aryanparashara <aryanparashara01@gmail.com>
2025-09-18 21:10:48 +0530

    Create 10sept2025.c

---
+++ 10sept2025.c
@@ -0,0 +1,25 @@
+/*#include<stdio.h>
+int main()
+{
+  int n,i,flag=0;
+  printf("\n enter a positive numbere: ");
+  scanf("%d",&n);
+  if((n==0)&&(n==1)) // 0 and 1 not prime numbers 
+  flag=1;
+  for(i=2;i<=n/2;i++)
+  {
+    if(n%i==0)
+    {
+      flag=1;
+      break;
+    }
+  }
+  if(flag==0)
+  {
+  printf("\n %d is a prime number ");
+  }
+  else 
+  {
+  printf("\n %d is not a prime number ");
+  }
+}*/
