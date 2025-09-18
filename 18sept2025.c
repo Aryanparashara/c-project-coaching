23d76783e18ab52ed28da06ebd51c0cf974d583e
Aryanparashara <aryanparashara01@gmail.com>
2025-09-18 21:59:30 +0530

    Create 18sept2025.c

---
+++ 18sept2025.c
@@ -0,0 +1,45 @@
+/*#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char a[20]="amit";
+  char b[20];
+  printf("\n Enter the password ");
+  gets(b);
+  if(strcmp(a,b)==0)
+  printf("\n password is correct ");
+  else 
+  printf("\n password is wrong ");
+}*/
+/*#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char a[20];
+  printf("\n enter the number");
+  gets(a);
+  printf("\n Reverse of your number is %d",strrev(a));
+}*/
+/*#include<stdio.h>
+#include<math.h>
+int main()
+{
+  int a,b,c;
+  printf("\n Enter two integers no.s ");
+  scanf("%d%d",&a,&b);
+  c=pow(a,b);
+  printf(" power=%d",c);
+  return 0;
+}*/
+/*#include<stdio.h>
+#include<math.h>
+int main()
+{
+  int n;
+  double x;
+  printf("\n Enter the Number ");
+  scanf("%d",&n);
+  x=sqrt(n);
+  printf("\n square root of %d is %1f",n,x);
+  return 0;
+}*/
