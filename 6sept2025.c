9e93e94b9903b91a02379e7a0fb8cf901fdcd5a6
Aryanparashara <aryanparashara01@gmail.com>
2025-09-18 21:09:37 +0530

    Create 6sept2025.c

---
+++ 6sept2025.c
@@ -0,0 +1,56 @@
+/*#include<stdio.h>
+electricity();
+int main()
+{
+  electricity();
+}
+electricity()
+{
+  int unit,bill;
+  printf("\n enter the unit ");
+  scanf("%d",&unit);
+  printf("\n monthly charges will be 550rupees ");
+  if((unit>=0)&&(unit<=100))
+  {
+    bill=(unit*5)+550;
+    printf("\n bill will be %d",bill);
+  }
+  else if ((unit>=100)&&(unit<=300))
+  {
+    bill=(unit-100)*8+(100*5)+550;
+    printf("\n bill will be %d",bill);
+  }
+  else 
+  {
+    bill=(unit-300)*11+(100*5)+(200*8)+550;
+    printf("\n bill will be %d",bill);
+  }
+}*/
+/*#include<stdio.h>
+int unit;
+void electricity()
+{
+  printf("\n enter the unit ");
+  scanf("%d",&unit);
+}
+void main()
+{
+  int bill;
+  printf("\n monthly charges will be 550 rupees ");
+  electricity();
+  if((unit>=0)&&(unit<=100))
+  {
+    bill=(unit*5)+550;
+    printf("\n bill will be %d",bill);
+  }
+  else if ((unit>=100)&&(unit<=300))
+  {
+    bill=(unit-100)*8+(100*5)+550;
+    printf("\n bill will be %d",bill);
+  }
+  else 
+  {
+    bill=(unit-300)*11+(100*5)+(200*8)+550;
+    printf("\n bill will be %d",bill);
+  }
+}*/
