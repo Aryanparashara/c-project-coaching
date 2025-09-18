fdcb9a08968faa8a1b517a08648dce38184096ac
Aryanparashara <aryanparashara01@gmail.com>
2025-09-18 21:12:13 +0530

    Create 13sept2025.c

---
+++ 13sept2025.c
@@ -0,0 +1,22 @@
+/*#include<stdio.h>
+int main()
+{
+  int cp,sp,amt;
+  printf("\n Enter the cost price: ");
+  scanf("%d",&cp);
+  printf("\n Enter the selling price: ");
+  scanf("%d",&sp);
+  if(sp>cp)
+  {
+    amt=sp-cp;
+    printf("\n profit=%d",amt);
+  }
+  else if(sp<cp)
+  {
+    printf("\n Loss=%d",amt);
+  }
+  else
+  {
+    printf(" No profit,No Loss");
+  }
+}*/
