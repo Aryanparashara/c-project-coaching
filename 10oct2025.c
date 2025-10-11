0f988543ed47a78547cef847faa1e817409893e8
Aryanparashara <aryanparashara01@gmail.com>
2025-10-11 22:38:32 +0530

    Create 10oct2025.c

---
+++ 10oct2025.c
@@ -0,0 +1,47 @@
+/*#include<stdio.h>
+int main()
+{
+  int i,j,c[2][2],matrix1[2][2],matrix2[2][2];
+  printf("\n Enter the elements : ");
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      printf("\n Enter elements [%d][%d]",i,j);
+      scanf("%d",&matrix1[i][j]);
+    }
+  }
+  printf("\n The entered matrix 1 is : \n ");
+  for(i=0;i<2;i++)
+  {
+    printf("%d",matrix1[i][j]);
+  }
+  printf("\n ");
+  printf("\n Enter the elements ");
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      printf("\n Enter elements [%d][%d]",i,j);
+      scanf("%d",&matrix2[i][j]);
+    }
+  }
+  printf("\n The entered matrix 2 is : \n ");
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      printf("%d",matrix2[i][j]);
+    }
+    printf("\n ");
+  }
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      c[i][j]=matrix1[i][j]+matrix2[i][j];
+      printf("\n %d",c[i][j]);
+    }
+  printf("\n ");
+  }
+}*/
