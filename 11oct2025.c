6dd196f11af55a898233342d8833896dc4084bd0
Aryanparashara <aryanparashara01@gmail.com>
2025-10-11 22:39:04 +0530

    Create 11oct2025.c

---
+++ 11oct2025.c
@@ -0,0 +1,32 @@
+/*#include<stdio.h>
+int main()
+{
+  int i,j,matrix[2][2];
+  printf("\n Enter the elements : ");
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      printf("\n Enter elements [%d][%d]",i,j);
+      scanf("%d",&matrix[i][j]);
+    }
+  }
+  printf("\n The entered matrix is : \n ");
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      printf("%d",matrix[i][j]);
+    }
+    printf("\n ");
+  }
+  printf("\n The transpose of matrix is : \n ");
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      printf("%d",matrix[j][i]);
+    }
+    printf("\n ");
+  }
+}*/
