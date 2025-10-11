d7b73559aa5631282650035a93fade8ee853bfc9
Aryanparashara <aryanparashara01@gmail.com>
2025-10-11 22:38:03 +0530

    Create 9oct2025.c

---
+++ 9oct2025.c
@@ -0,0 +1,24 @@
+// Matrix format 
+/*#include<stdio.h>
+int main()
+{
+  int i,j,matrix[2][2];
+  printf("\n Enter the elements ");
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      printf("\n Enter elements [%d][%d]",i,j);
+      scanf("%d",&matrix[i][j]);
+    }
+  }
+  printf("\n The Entered matrix is : \n ");
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      printf("%d",matrix[i][j]);
+    }
+    printf("\n ");
+  }
+}*/
