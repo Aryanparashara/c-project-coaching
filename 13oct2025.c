b3148ae03b56f8e71d75f87b1d5d3fee67d98d02
Aryanparashara <aryanparashara01@gmail.com>
2025-10-14 21:45:46 +0530

    Create 13oct2025.c

---
+++ 13oct2025.c
@@ -0,0 +1,24 @@
+/*#include<stdio.h>
+int main()
+{
+  int array[100];
+  int i,n,x,flag=0;
+  printf("\n Enter size of an array  ");
+  scanf("%d",&n);
+  printf("\n Enter the elements ");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&array[i]);
+  }
+  printf("\n Enter the element you want to search ");
+  scanf("%d",&x);
+  for(i=0;i<n;i++)
+  {
+    if(x==array[i])
+    flag=1;
+  }
+  if(flag==1)
+  printf("\n Element found ");
+  else 
+  printf("\n Element not found ");
+}*/
